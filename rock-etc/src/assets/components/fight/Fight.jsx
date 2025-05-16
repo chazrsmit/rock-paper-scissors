ad935ce52cca8d0a27601e14d733abfe02e5820f
Charlotte <smit-charlotte@outlook.com>
2025-05-16 10:42:15 +0200

    update responsive figth

--- rock-etc/src/assets/components/fight/Fight.jsx
+++ rock-etc/src/assets/components/fight/Fight.jsx
@@ -6,53 +6,83 @@ import React, { useEffect, useState } from "react";
 
 export default function Fight({ ident, random, inFight, setRandom, score, setScore }) {
 
+    // Pour gérer la "barre" de chargement
     const [isLoading, setIsLoading] = useState(true)
+
+    // Pour afficher le message "You win / You lose"
+    const [result, setResult] = useState("")
+
     // Pour l'utilisation d'un setTimeOut, obligation d'utiliser un useEffect (en effet, en le mettant dans le return, ça peut créer des bugs.)
     useEffect(() => {
-    const timer = setTimeout(() => {
+        const timer = setTimeout(() => {
 
-    // Arrêter l'effet de chargement après 4sec grâce à ce switch on/off (true/false).
-    setIsLoading(false);
-    
-    // Une fois qu'un combat est lancé, on va générer un random number correspondant à l'un des 3 choix possibles (0: paper, 1: scissors, 2: rock).
-    if (inFight === true) {
-        setRandom(random = Math.floor(Math.random()*3))
-        console.log(random)
-    }
+        // Arrêter l'effet de chargement après 4sec grâce à ce switch on/off (true/false).
+        setIsLoading(false);
+        
+        // Une fois qu'un combat est lancé, on va générer un random number correspondant à l'un des 3 choix possibles (0: paper, 1: scissors, 2: rock).
+        if (inFight === true) {
+            setRandom(random = Math.floor(Math.random()*3))
+            console.log(random)
+        }
 
-    // Calcul des points suite au résultat du combat :
-    // On le met dans le setTimeOut pour éviter des bugs.
-    if (ident === "paper") {
-        if (random === 0) {
-            console.log("draw")
+        // Calcul des points suite au résultat du combat :
+        // On le met dans le setTimeOut pour éviter des bugs.
+        if (ident === "paper") {
+            if (random === 0) {
+                console.log("draw")
+                setResult("draw")
+            }
+            else if (random === 1) {
+                console.log("you lose")
+                setScore(score = score - 1)
+                setResult("lose")
+            }
+            else {
+                console.log("you win")
+                setScore(score = score + 1)
+                setResult("win")
+            }
         }
-        else if (random === 1) {
-            console.log("you lose")
-            setScore(score = score - 1)
-        }
-        else {
-            console.log("you win")
-            setScore(score = score + 1)
-        }
-    }
 
-    if (ident === "scissors") {
-        if (random === 0) {
-            console.log("you win")
-            setScore(score = score + 1)
+        if (ident === "scissors") {
+            if (random === 0) {
+                console.log("you win")
+                setScore(score = score + 1)
+                setResult("win")
+            }
+            else if (random === 1) {
+                console.log("draw")
+                setResult("draw")
+            }
+            else {
+                console.log("you lose")
+                setScore(score = score - 1)
+                setResult("lose")
+            }
         }
-        else if (random === 1) {
-            console.log("draw")
-        }
-        else {
-            console.log("you lose")
-            setScore(score = score - 1)
-        }
-    }
 
-    }, 3000);
-    // Cleanup (recommandé)
-    return () => clearTimeout(timer);
+        if (ident === "rock") {
+            if (random === 0) {
+                console.log("you lose")
+                setScore(score = score - 1)
+                setResult("lose")
+            }
+            else if (random === 1) {
+                console.log("you win")
+                setScore(score = score + 1)
+                setResult("win")
+            }
+            else {
+                console.log("draw")
+                setResult("draw")
+            }
+        }
+
+        }, 3000);
+
+        // Cleanup (recommandé)
+        return () => clearTimeout(timer);
+
     }, []); 
 
     return(
@@ -91,16 +121,34 @@ export default function Fight({ ident, random, inFight, setRandom, score, setSco
 
                 </div>
 
-                {/* Chargement/loading (disparait après 3sec grâce au setTimeout plus haut) */}
+                {/* Sur ordis */}    
+                
+                <div className="div-milieu col-3">
 
-                {isLoading && (
-                    <div className="lds-ellipsis">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        {/* <div></div> */}
-                    </div>
-                )}
+                    {isLoading && (
+                        <div className="lds-ellipsis">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    )}
+
+                    {result === "win" &&
+                        <h4>YOU WIN</h4>
+                    }
+                    {result === "lose" &&
+                        <h4>YOU LOSE</h4>
+                    }
+                    {result === "draw" &&
+                        <h4>DRAW</h4>
+                    }
+
+                    {!isLoading && 
+                        <button>Play again?</button>
+                    }
+
+                </div>
+
 
                 <div className="house-choice d-flex flex-column gap-5 align-items-center">
                     <div className="title-choice">
@@ -139,6 +187,38 @@ export default function Fight({ ident, random, inFight, setRandom, score, setSco
 
                 </div>
 
+            </div>
+
+            <div className="div-fight2">
+
+                {/* Sur tablettes et téléphones */}
+                <div className="div-milieu2 col-3">
+
+                    {isLoading && (
+                        <div className="lds-ellipsis">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            {/* <div></div> */}
+                        </div>
+                    )}
+
+                    {/* Remplacé ensuite par le résultat après 3 sec */}
+                    {result === "win" &&
+                        <h4>YOU WIN</h4>
+                    }
+                    {result === "lose" &&
+                        <h4>YOU LOSE</h4>
+                    }
+                    {result === "draw" &&
+                        <h4>DRAW</h4>
+                    }
+
+                    {!isLoading && 
+                        <button>Play again?</button>
+                    }
+
+                </div>
             </div>
         </>
     )
